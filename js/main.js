0f5969e0b893cf967638666ac351530617fe0034
suneel kumar yarramuthala <yarramuthalasunil@gmail.com>
2022-10-25 19:52:56 +0530

    added the functionality to get name from the users

--- js/main.js
+++ js/main.js
@@ -1,9 +1,5 @@
-var btn = document.querySelector("#btn");
-let score = 0;
+var ok = document.querySelector("#btn1");
 
-function goToQuiz() {
-  location.replace("quiz.html");
-}
 $ = function (id) {
   return document.getElementById(id);
 };
@@ -15,28 +11,21 @@ var hide = function (id) {
   $(id).style.display = "none";
 };
 
-btn.addEventListener("click", () => {
-  var q1 = document.querySelector('input[name="q1"]:checked');
-  var q2 = document.querySelector('input[name="q2"]:checked');
-  var q3 = document.querySelector('input[name="q3"]:checked');
-  var q4 = document.querySelector('input[name="q4"]:checked');
-  console.log(q1.value);
-  console.log(q2.value);
-  console.log(q3.value);
-  console.log(q4.value);
-  console.log(typeof q1.value);
-  if (q1.value === "1") {
-    score += 1;
+ok.addEventListener("click", () => {
+  var name = document.querySelector('input[name="name"]');
+  console.log(name.value);
+  console.log(name.value.length);
+  if (name.value === null) {
+    alert("Please enter a name to move further");
+  } else {
+    if (name.value === "") {
+      alert("Please enter a name to move further");
+    } else {
+      if (name.value.length < 3) {
+        alert("Please enter a name with more than two characters");
+      } else {
+        location.replace("quiz.html");
+      }
+    }
   }
-  if (q2.value === "2") {
-    score += 1;
-  }
-  if (q3.value === "3") {
-    score += 1;
-  }
-  if (q4.value === "4") {
-    score += 1;
-  }
-  alert("your score is " + score);
-  location.reload();
 });
---
+++ js/quiz.js
@@ -0,0 +1,27 @@
+var btn = document.querySelector("#btn");
+let score = 0;
+btn.addEventListener("click", () => {
+  var q1 = document.querySelector('input[name="q1"]:checked');
+  var q2 = document.querySelector('input[name="q2"]:checked');
+  var q3 = document.querySelector('input[name="q3"]:checked');
+  var q4 = document.querySelector('input[name="q4"]:checked');
+  console.log(q1.value);
+  console.log(q2.value);
+  console.log(q3.value);
+  console.log(q4.value);
+  console.log(typeof q1.value);
+  if (q1.value === "1") {
+    score += 1;
+  }
+  if (q2.value === "2") {
+    score += 1;
+  }
+  if (q3.value === "3") {
+    score += 1;
+  }
+  if (q4.value === "4") {
+    score += 1;
+  }
+  alert("your score is " + score);
+  location.reload();
+});
